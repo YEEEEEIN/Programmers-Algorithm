68e6d830f0a6316eaa34e2fcf75a728232e1b01c
YEEEEEIN <129138914+YEEEEEIN@users.noreply.github.com>
2023-05-13 00:01:55 +0900

    알고리즘 문풀

---
+++ Day230513/src/Day230513/Solution.java
@@ -0,0 +1,27 @@
+package Day230513;
+
+class Solution {
+	public int solution(String[] babbling) {
+		int answer = 0;
+
+		for (int i = 0; i < babbling.length; i++) {
+			babbling[i] = babbling[i].replaceFirst("aya", "0");
+			babbling[i] = babbling[i].replaceFirst("woo", "0");
+			babbling[i] = babbling[i].replaceFirst("ye", "0");
+			babbling[i] = babbling[i].replaceFirst("ma", "0");
+			babbling[i] = babbling[i].replace("0", "");
+
+			if (babbling[i].equals(""))
+				answer++;
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.solution(new String[] { "aya", "yee", "u", "maa", "wyeoo" }));
+		System.out.println(s.solution(new String[] { "ayaye", "uuuma", "ye", "yemawoo", "ayaa" }));
+
+	}
+}
