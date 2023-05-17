371bcba9ab303f63ef3680a3f65ea04060e920fc
YEEEEEIN <129138914+YEEEEEIN@users.noreply.github.com>
2023-05-18 01:37:06 +0900

    자바 알고리즘 문풀

--- Day230517/src/다음에_올_숫자/Solution.java
+++ Day230517/src/다음에_올_숫자/Solution.java
@@ -2,7 +2,7 @@
  * 등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 
  * 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
  */
-package ss;
+package 다음에_올_숫자;
 
 class Solution {
 	public int solution(int[] common) {
