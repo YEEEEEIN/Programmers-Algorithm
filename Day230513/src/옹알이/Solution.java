1e0926d14f727f04f99fd484cdd2cefda16a0330
YEEEEEIN <129138914+YEEEEEIN@users.noreply.github.com>
2023-05-17 23:58:10 +0900

    replace 코드 수정

--- Day230513/src/옹알이/Solution.java
+++ Day230513/src/옹알이/Solution.java
@@ -10,10 +10,10 @@ class Solution {
 		int answer = 0;
 
 		for (int i = 0; i < babbling.length; i++) {
-			babbling[i] = babbling[i].replaceFirst("aya", "0");
-			babbling[i] = babbling[i].replaceFirst("woo", "0");
-			babbling[i] = babbling[i].replaceFirst("ye", "0");
-			babbling[i] = babbling[i].replaceFirst("ma", "0");
+			babbling[i] = babbling[i].replace("aya", "0");
+			babbling[i] = babbling[i].replace("woo", "0");
+			babbling[i] = babbling[i].replace("ye", "0");
+			babbling[i] = babbling[i].replace("ma", "0");
 			babbling[i] = babbling[i].replace("0", "");
 
 			if (babbling[i].equals(""))
